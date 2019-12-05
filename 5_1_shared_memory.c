2d7c96c84b3b1dcd0c4067c24b6758dc26b4f331
vrlevc <levchenko_victor@gmail.com>
2019-12-05 19:29:34 +0200

    5.1 use shared memory - done

--- 5_1_shared_memory.c
+++ 5_1_shared_memory.c
@@ -5,7 +5,44 @@
 int main ()
 {
     int segment_id;
-    
+    char* shared_memory;
+    struct shmid_ds shmbuffer;
+    int segment_size;
+    const int shared_segment_size = 0x6400;
+
+    /** Allocate a shared memory segment. */
+    segment_id = shmget (IPC_PRIVATE, shared_segment_size,
+                         IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+
+    /** Attach shared memory segment. */
+    shared_memory = (char*) shmat (segment_id, NULL, 0);
+    printf (" --- shared memory attached at address %p\n", shared_memory);
+
+    /** Determine the segment's size. */
+    shmctl (segment_id, IPC_STAT, &shmbuffer);
+    segment_size = shmbuffer.shm_segsz;
+    printf (" --- segment size: %d\n", segment_size);
+
+    /** Write a string to the shared memory segment. */
+    sprintf (shared_memory, "Hello, world.");
+
+    /** Detach the shared memory segment. */
+    shmdt (shared_memory);
+
+
+    /** Reattach the shared memory segment, at a different address. */
+    shared_memory = (char*) shmat (segment_id, (void*) 0x5000000, 0);
+    printf (" --- shared memory reattached at address %p\n", shared_memory);
+
+    /** Print out the string from shared memory. */
+    printf ("%s\n", shared_memory);
+
+    /** Detach the shared memory segment. */
+    shmdt (shared_memory);
+
+
+    /** Deallocate the shared memory segment. */
+    shmctl (segment_id, IPC_RMID, NULL);
 
     return 0;
 }
